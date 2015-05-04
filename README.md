2ccce36002d80406575487b75d15e4bd2334568a
piotrsikora <fedojo.com@gmail.com>
2015-05-04 23:05:57 +0200

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+# FEDojoBoilerplate 
+FEDojo starter kit
+
+[http://dev.fedojo.com/](http://fedojo.com/)
+
+## Current state
+version BETA
+
+
+## Change log
+### 0.0.0 Init (4.05.2015)
+- Idea + initial code
+- Gulp
+- Initial structure of sass and jade files
+
---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    jade = require('gulp-jade'),
+    minifyCSS = require('gulp-minify-css'),
+    rename    = require('gulp-rename'),
+    server = require('gulp-server-livereload'),
+    notify = require('gulp-notify'); 
+
+
+var paths = {
+  css: 'src/sass/*',
+  jade: 'src/templates/*',
+  js: 'src/js/*',
+};
+
+
+gulp.task('watch', function() {
+  gulp.watch(paths.css, ['sass']);
+  gulp.watch(paths.jade, ['jade']);
+});
+
+
+gulp.task('sass', function() {
+  gulp
+  .src('./src/sass/*.scss')
+  .pipe(sass({
+    includePaths: ['bower_components/breakpoint-sass/stylesheets'],
+    includePaths: ['bower_components/MultiGrid/stylesheets']
+  }))
+  .pipe(gulp.dest('./dist/css'))
+  .pipe(notify("SASS done"));
+});
+
+
+gulp.task('jade', function() {
+  var YOUR_LOCALS = {};
+
+  gulp
+  .src('./src/templates/*.jade')
+  .pipe(jade({
+    locals: YOUR_LOCALS
+  })
+  .on('error', function(err) {
+    console.log(err);
+  }))
+
+  .pipe(gulp.dest('./dist/'))
+  .pipe(notify("JADE done"))
+});
+
+gulp.task('default', ['watch', 'sass', 'jade']);
